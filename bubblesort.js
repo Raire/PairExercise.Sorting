e8c72cd6d8f99dad5671da8763c049d04b7568a3
AsherLMac <renpsybeta@gmail.com>
2018-06-11 13:56:39 -0400

    Done

--- bubblesort.js
+++ bubblesort.js
@@ -1,12 +1,12 @@
 function bubbleSort(array) {
-  let temp;
-  let state;
+  let temp; //Holds Temporary Order Of Swap
+  let state; //Determine if a swap has occurred
   for (let i = 0; i < array.length; i++) {
-    if (array[i + 1]) {
-      temp = swap(array[i], array[i + 1]);
+    if (array[i + 1]) { //If Next Is Not Falsely
+      temp = swap(array[i], array[i + 1]); //Hold The Swap
       array[i] = temp[0];
       array[i + 1] = temp[1];
-      if (temp[2]) {
+      if (temp[2]) { //Checks If Swapped;
         state = temp[2];
       }
     }
@@ -17,6 +17,7 @@ function bubbleSort(array) {
   return array;
 }
 
+//Determine The Swap
 function swap(num1, num2) {
   let temp = [num1, num2];
   if (num1 > num2) {
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,21 +1,23 @@
-/*beforeAll(function() {
-  spyOn(window, "swap").and.callThrough();
-});
-it("swaps the right number of times", function() {});
-*/
 describe("Bubble Sort", function() {
+  beforeAll(function() {
+    spyOn(window, 'swap').and.callThrough();
+  });
+
+  it("swaps the right number of times", function() {
+    bubbleSort([1,2,3,4]);
+    expect(window.swap.calls.count()).toEqual(3);
+  });
+
   it("handles an empty array", function() {
     expect(bubbleSort([])).toEqual([]);
   });
-});
 
-describe("Bubble Sort", function() {
   it("sorts the array", function() {
     expect(bubbleSort([2, 3, 5, 1])).toEqual([1, 2, 3, 5]);
   });
-});
 
-describe("Bubble Sort", function() {
+
+
   it("handles only one element in array", function() {
     expect(bubbleSort([1])).toEqual([1]);
   });
